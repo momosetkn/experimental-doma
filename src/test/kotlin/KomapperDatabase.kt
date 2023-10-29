32c2dcb7afa5b6a7670b456347c2a637fe449ce7
t.momose <hyakkun@gmail.com>
2023-10-29 22:23:38 +0900

    Create Komapper test support

---
+++ src/test/kotlin/KomapperDatabase.kt
@@ -0,0 +1,95 @@
+import momosetkn.infras.komapper.repositories.DatabaseDao
+import org.komapper.annotation.KomapperId
+import org.komapper.core.dsl.QueryDsl
+import org.komapper.core.dsl.metamodel.EntityMetamodel
+import org.komapper.core.dsl.metamodel.PropertyMetamodel
+import org.komapper.core.dsl.query.EntitySelectQuery
+import org.komapper.jdbc.JdbcDatabase
+import kotlin.reflect.KCallable
+import kotlin.reflect.KClass
+import kotlin.reflect.full.hasAnnotation
+import kotlin.reflect.jvm.isAccessible
+
+object KomapperDatabase {
+    private val IGNORE_TABLE_LIST = listOf(
+        // Liquibase
+        "DATABASECHANGELOG",
+        "DATABASECHANGELOGLOCK",
+    )
+
+    fun <ENTITY : Any, ID : Any, META : EntityMetamodel<ENTITY, ID, META>>
+        JdbcDatabase.createList(meta: META, items: List<ENTITY>): List<ENTITY> {
+        return runQuery(
+            QueryDsl.insert(meta).batch(items),
+        )
+    }
+
+    fun <ENTITY : Any, ID : Any, META : EntityMetamodel<ENTITY, ID, META>>
+        JdbcDatabase.create(meta: META, item: ENTITY): ENTITY {
+        return runQuery(
+            QueryDsl.insert(meta).single(item),
+        )
+    }
+
+    fun <ENTITY : Any, ID : Any, META : EntityMetamodel<ENTITY, ID, META>>
+        JdbcDatabase.reloadList(meta: META, items: List<ENTITY>): List<ENTITY> {
+        val propertyMetaToValues = getIdFields(items[0]::class).map { field ->
+
+            val member = requireNotNull(getMemberByName(meta::class, field.name))
+            val metaMember = member.call(meta) as PropertyMetamodel<ENTITY, Any, Any>
+
+            Pair(
+                metaMember,
+                items.map { field.call(it) },
+            )
+        }
+
+        val results = run {
+            var query: EntitySelectQuery<ENTITY> = QueryDsl.from(meta)
+
+            query = propertyMetaToValues.fold(query) { acc, metaToValue ->
+                acc.where { metaToValue.first inList metaToValue.second }
+            }
+
+            query
+        }
+
+        return runQuery(results)
+    }
+
+    fun <ENTITY : Any, ID : Any, META : EntityMetamodel<ENTITY, ID, META>> JdbcDatabase.reload(
+        meta: META,
+        item: ENTITY,
+    ): ENTITY {
+        return reloadList(meta, listOf(item))[0]
+    }
+
+    fun <ENTITY : Any, ID : Any, META : EntityMetamodel<ENTITY, ID, META>> JdbcDatabase.all(meta: META): List<ENTITY> {
+        return runQuery {
+            QueryDsl.from(meta)
+        }
+    }
+
+    fun JdbcDatabase.reset() {
+        runQuery(QueryDsl.executeScript(("set foreign_key_checks = 0")))
+        DatabaseDao(this).showTables().filter { !IGNORE_TABLE_LIST.contains(it) }.forEach {
+            runQuery(QueryDsl.executeScript(("truncate table $it")))
+        }
+        runQuery(QueryDsl.executeScript("set foreign_key_checks = 1"))
+    }
+
+    private fun getIdFields(kclazz: KClass<out Any>): List<KCallable<*>> {
+        return kclazz.members.filter { declaredField ->
+            declaredField.hasAnnotation<KomapperId>()
+        }.map {
+            it.isAccessible = true
+            it
+        }
+    }
+
+    private fun getMemberByName(kclass: KClass<out Any>, memberName: String): KCallable<*>? {
+        return kclass.members.find {
+            it.name == memberName
+        }
+    }
+}
---
+++ src/test/kotlin/KomapperExampleTest.kt
@@ -0,0 +1,39 @@
+import KomapperDatabase.all
+import KomapperDatabase.create
+import KomapperDatabase.createList
+import KomapperDatabase.reload
+import KomapperDatabase.reset
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import momosetkn.app.Koin
+import momosetkn.app.appDependencies
+import momosetkn.infras.komapper.entities.InfraCompanies
+import momosetkn.infras.komapper.entities.infraCompanies
+import momosetkn.infras.komapper.komapper.contexts.Db
+import org.koin.core.component.get
+import org.koin.core.context.startKoin
+import org.komapper.core.dsl.Meta
+
+class KomapperExampleTest : FunSpec({
+    startKoin { modules(appDependencies) }
+
+    val db = Koin.get<Db>().jdbcDatabase
+
+    beforeEach {
+        db.reset()
+    }
+    test("test") {
+        val item = InfraCompanies(
+            id = "1",
+            name = "name1",
+            updatedBy = "updatedBy",
+            updatedAt = java.time.LocalDateTime.of(2021, 1, 1, 1, 1, 1),
+            createdBy = "createdBy",
+            createdAt = java.time.LocalDateTime.of(2021, 1, 1, 1, 1, 2),
+        )
+        db.withTransaction {
+            db.create(Meta.infraCompanies, item)
+            db.reload(Meta.infraCompanies, item) shouldBe item
+        }
+    }
+})
