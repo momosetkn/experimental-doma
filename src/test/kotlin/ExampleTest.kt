6781082f2d1c25a364c45b3b6a2a3b1037670365
t.momose <hyakkun@gmail.com>
2023-10-29 21:37:14 +0900

    Add ExampleTest

---
+++ src/test/kotlin/ExampleTest.kt
@@ -0,0 +1,36 @@
+import Database.create
+import Database.reload
+import Database.reset
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import momosetkn.app.Koin
+import momosetkn.app.appDependencies
+import momosetkn.infras.doma.doma.contexts.Db
+import momosetkn.infras.doma.entities.InfraCompanies
+import momosetkn.infras.doma.entities.meta.Meta
+import momosetkn.infras.doma.entities.meta.companies
+import org.koin.core.component.get
+import org.koin.core.context.startKoin
+
+class ExampleTest : FunSpec({
+    startKoin { modules(appDependencies) }
+
+    val db = Koin.get<Db>()
+    val context = db.getContext()
+
+    beforeEach {
+        context.reset()
+    }
+    test("test") {
+        val item = InfraCompanies(
+            id = "1",
+            name = "name1",
+            updatedBy = "updatedBy1",
+            updatedAt = java.time.LocalDateTime.of(2021, 1, 1, 1, 1, 1),
+            createdBy = "createdBy",
+            createdAt = java.time.LocalDateTime.of(2021, 1, 1, 1, 1, 2),
+        )
+        context.create(Meta.companies, item)
+        context.reload(Meta.companies, item) shouldBe item
+    }
+})
